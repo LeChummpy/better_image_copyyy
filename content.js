5b58ea86fbdf798a136773c4b9c01416b664ce05
LeChummpy <benjamin.jank.ges@gmx.de>
2020-12-11 18:04:18 +0100

    wawnjdawd

--- content.js
+++ content.js
@@ -1,22 +1,18 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-let all_images=[];
 var observer = new MutationObserver(function(mutations, observer) {
     // fired when a mutation occurs
     for (mutation of mutations){
-      //console.log(mutation);
-        if (mutation.target.dataset.id!==undefined) {
+        if (mutation.target.classList[0]=="isv-r") { //bei jedem neu geladenen Bild
             console.log(mutation.target.dataset.id);
-            //let element = document.querySelectorAll('[data-ID="' + mutation.target.dataset.id + '"]');
-            let pic_divs = document.getElementById("islrg").getElementsByClassName("islrc")[0].childNodes;
-            console.log(pic_divs);
+            let pic_div = document.getElementById("islrg").getElementsByClassName("islrc")[0].childNodes; //alle pic_divs werden ausgewählt (Bild + Beschreibungstext)
         }
     }
 });
 
 // define what element should be observed by the observer
 // and what types of mutations trigger the callback
-var body_container = document.documentElement || document.body;
+var body_container = document.body;
 observer.observe(body_container, {
   subtree: true,
   attributes: true
