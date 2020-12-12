1221e7ad250fd42731089cb11ae861690eba724b
LeChummpy <benjamin.jank.ges@gmx.de>
2020-12-12 18:34:41 +0100

    now working fine, popup ready to implement

--- content.js
+++ content.js
@@ -1,31 +1,59 @@
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-var observer = new MutationObserver(function(mutations, observer) {
-    // fired when a mutation occurs
-    for (mutation of mutations){
-        if (mutation.target.classList[0]=="isv-r" && mutation.target.classList[1]=="PNCib" && mutation.target.classList[2]=="MSM1fd") { //bei jedem neu geladenen Bild
-            let pic_div = get_image_div().querySelectorAll('[data-ID="' + mutation.target.dataset.id + '"]')[0]; //alle pic_divs werden ausgewählt (Bild + Beschreibungstext)
+      var image_div_children = get_image_div().children;
+      for (i of image_div_children) {
+        i.addEventListener("mouseenter", function() {
+            div_mouseover_event(i);
+        });
 
-            pic_div.addEventListener("mouseenter", function() {
-                pic_div.style.border = '5px dotted orange';
-            });
+        i.addEventListener("mouseleave", function() {
+          div_mouseleave_event(i);
+        });
+      }
 
-            pic_div.addEventListener("mouseleave", function() {
-              pic_div.style.border = null;
-            });
 
+      MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+      var observer = new MutationObserver(function(mutations, observer) {
+          // fired when a mutation occurs
+          for (mutation of mutations){
+              if (mutation.target.classList[0]=="isv-r" && mutation.target.classList[1]=="PNCib" && mutation.target.classList[2]=="MSM1fd") { //bei jedem neu geladenen Bild
+                  let pics_div = get_image_div()
+                  let pic_div = pics_div.querySelectorAll('[data-ID="' + mutation.target.dataset.id + '"]')[0]; //alle pic_divs werden ausgewählt (Bild + Beschreibungstext)
+
+                  if ( !(pic_div===undefined) ) {
+                  pic_div.addEventListener("mouseenter", function() {
+                      div_mouseover_event(pic_div);
+                  });
+
+                  pic_div.addEventListener("mouseleave", function() {
+                    div_mouseleave_event(pic_div);
+                  });
+                }
+            }
+          }
+      });
+
+      var body_container = document.body;
+      observer.observe(body_container, {
+        subtree: true,
+        attributes: true
+      });
+      //-----------------------------------------------helping functions-------------------------------------------------------------------------------------
+
+      function div_mouseover_event(div) {
+        div.style.border = '1px dotted red';
+      }
+
+      function div_mouseleave_event(div) {
+        div.style.border = '1px dotted white';;
+      }
+
+      function get_image_div() {
+        let image_div = document.getElementById("islrg");
+        if (image_div!==null) {
+          let image_div_lower = image_div.getElementsByClassName("islrc")[0];
+          return image_div_lower;
+        } else {
+          return null
         }
-    }
-});
-
-var body_container = document.body;
-observer.observe(body_container, {
-  subtree: true,
-  attributes: true
-});
-//-----------------------------------------------helping functions-------------------------------------------------------------------------------------
-
-function get_image_div() {
-  let image_div = document.getElementById("islrg").getElementsByClassName("islrc")[0];
-  return image_div;
-}
+      }
