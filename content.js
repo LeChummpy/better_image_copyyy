e6a6a0e5955ae990ca1c14b5fb66dfeac4496253
LeChummpy <benjamin.jank.ges@gmx.de>
2020-12-12 19:43:32 +0100

    made some changes  Please enter the commit message for your changes. Lines starting

--- content.js
+++ content.js
@@ -1,32 +1,34 @@
 
-      var image_div_children = get_image_div().children;
-      for (i of image_div_children) {
-        i.addEventListener("mouseenter", function() {
-            div_mouseover_event(i);
-        });
 
-        i.addEventListener("mouseleave", function() {
-          div_mouseleave_event(i);
-        });
-      }
+      let image_div = get_image_div();
+      if ( !(image_div===null) ) {
+          let image_div_children = image_div.children;
+          for (i of image_div_children) {
+            i.addEventListener("mouseenter", function() {
+                div_mouseover_event(i);
+            });
+
+            i.addEventListener("mouseleave", function() {
+              div_mouseleave_event(i);
+            });
+          }
+        }
 
 
       MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
       var observer = new MutationObserver(function(mutations, observer) {
           // fired when a mutation occurs
           for (mutation of mutations){
               if (mutation.target.classList[0]=="isv-r" && mutation.target.classList[1]=="PNCib" && mutation.target.classList[2]=="MSM1fd") { //bei jedem neu geladenen Bild
                   let pics_div = get_image_div()
                   let pic_div = pics_div.querySelectorAll('[data-ID="' + mutation.target.dataset.id + '"]')[0]; //alle pic_divs werden ausgewählt (Bild + Beschreibungstext)
-
                   if ( !(pic_div===undefined) ) {
-                  pic_div.addEventListener("mouseenter", function() {
-                      div_mouseover_event(pic_div);
-                  });
+                      pic_div.addEventListener("mouseenter", function() {
+                          div_mouseover_event(pic_div);
+                      });
 
-                  pic_div.addEventListener("mouseleave", function() {
-                    div_mouseleave_event(pic_div);
+                      pic_div.addEventListener("mouseleave", function() {
+                        div_mouseleave_event(pic_div);
                   });
                 }
             }
@@ -38,14 +40,15 @@
         subtree: true,
         attributes: true
       });
+
       //-----------------------------------------------helping functions-------------------------------------------------------------------------------------
 
       function div_mouseover_event(div) {
-        div.style.border = '1px dotted red';
+          div.innerHTML="";
       }
 
       function div_mouseleave_event(div) {
-        div.style.border = '1px dotted white';;
+
       }
 
       function get_image_div() {
