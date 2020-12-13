dd2e8d0ba97b281e3f1224239e5d7091a9184242
LeChummpy <benjamin.jank.ges@gmx.de>
2020-12-13 02:12:12 +0100

    wdawdawd

--- content.js
+++ content.js
@@ -36,7 +36,7 @@ let class_specification2="MSM1fd";
 
       function prepare_div(pic_div) {
         let prepared_fragezeichen = pic_div.getAttribute("data-prepared");
-        console.log(prepared_fragezeichen);
+
         if( prepared_fragezeichen==="false" || prepared_fragezeichen===undefined || prepared_fragezeichen===null) { //wenn div noch nicht prepared
           let button = document.createElement("BUTTON");
           button.style.position = "absolute";
@@ -44,6 +44,7 @@ let class_specification2="MSM1fd";
           button.style.top=0;
           button.style.width="25px";
           button.style.height="25px";
+          button.style.display="none";
           pic_div.appendChild(button);
 
           pic_div.addEventListener("mouseenter", function() {
@@ -53,18 +54,24 @@ let class_specification2="MSM1fd";
           pic_div.addEventListener("mouseleave", function() {
             div_mouseleave_event(pic_div);
       });
-      let attribute = document.createAttribute("data-prepared"); //div als prepared markiert
-      attribute.value = "true";
-      pic_div.setAttributeNode(attribute);
+          let attribute = document.createAttribute("data-prepared"); //div als prepared markiert
+          attribute.value = "true";
+          pic_div.setAttributeNode(attribute);
     }
   }
 
       function div_mouseover_event(div) {
-          //div.style.background="green";
+          let btns = div.getElementsByTagName("button");
+          for (i of btns) {
+            i.style.display="block";
+          }
       }
 
       function div_mouseleave_event(div) {
-          //div.style.background=null;
+        let btns = div.getElementsByTagName("button");
+        for (i of btns) {
+          i.style.display="none";
+        }
       }
 
       function get_image_div() {
