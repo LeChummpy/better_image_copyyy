8f8075146399e09fb70bc432f09d323605bbba1d
LeChummpy <benjamin.jank.ges@gmx.de>
2020-12-16 19:49:19 +0100

    tab focusable and refocusable

--- background.js
+++ background.js
@@ -1,11 +1,19 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) { //if new page is loaded
     if (msg.text == "focus me!") {
         focused_tab = sender.tab.id;
-        sendResponse("You have been focused!");
+          sendResponse({});
+          chrome.tabs.sendMessage(focused_tab, {text:"You have been focused!"});
 
-        chrome.downloads.onCreated.addListener(function(item) {
-          chrome.tabs.sendMessage(focused_tab, {text:"item downloaded.", downloaded_item:item});
-            //chrome.downloads.cancel(item.id);
+          chrome.downloads.onCreated.addListener(function(item) {
+            chrome.tabs.sendMessage(focused_tab, {text:"item downloaded.", downloaded_item:item});
 });
     }
+});
+
+chrome.tabs.onActiveChanged.addListener(function(tabID) {
+  chrome.tabs.sendMessage(tabID, {text:"You have been refocused!"});
+
+  chrome.downloads.onCreated.addListener(function(item) {
+    chrome.tabs.sendMessage(tabID, {text:"item downloaded.", downloaded_item:item});
+});
 });
--- content.js
+++ content.js
@@ -1,15 +1,22 @@
-chrome.runtime.sendMessage({text:"focus me!"}, function (response) {
-  if(response==="You have been focused!") {
-    console.log("This tab was focused by background script.");
-  }
-});
+chrome.runtime.sendMessage({text:"focus me!"}, function (response) {});
 
 chrome.runtime.onMessage.addListener(function(msg) {
     if (msg.text=="item downloaded.") {
+      if (msg.downloaded_item.mime==="image/jpeg" || msg.downloaded_item.mime==="image/png" ) {
       let clipboardcontent = get_clipboard_content(msg.downloaded_item);
       copy_to_clipboard(clipboardcontent);
     }
+
+  } else if(msg.text=="You have been refocused!") {
+    console.log("This tab was refocused by background script.");
+
+  } else if(msg.text=="You have been focused!") {
+    console.log("This tab was focused by background script.");
+
+  }
 });
+
+//--------------------------helping functions---------------------------------------------
 
 function get_clipboard_content(downloadItem) {
   src = downloadItem.url;
