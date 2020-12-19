4a87331c0e0aa2c270efc156a16de3339a0e2442
LeChummpy <benjamin.jank@gmx.de>
2020-12-19 12:59:48 +0100

    now working for all pages

--- content.js
+++ content.js
@@ -20,37 +20,37 @@ document.addEventListener("dragstart", function(event){
   let dragged_element = event.target;
   if (dragged_element.nodeName==="IMG") {
     let clipboardcontent = get_clipboard_content_from_HTMLImage(dragged_element);
+    j.appendChild(document.createElement("BUTTON"));
     copy_to_clipboard(clipboardcontent);
   }
 });
 
-let class_specification="eHAdSb";
+let imgs =document.getElementsByTagName("IMG"); //auch vorgeladene Bilder preparieren
+for (i of imgs) {
+  prepare_img(i);
+  mark_img_as_prepared(i);
+}
+
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-var observer = new MutationObserver(function(mutations, observer) {
+var observer = new MutationObserver(function(mutations, observer) { //alle neu geladenen Bilder als prepariert markieren
     // fired when a mutation occurs
-    for (mutation of mutations){
-      //console.log(mutation.target)
-        if (mutation.target.getAttribute("class")===class_specification) { //bei jedem neu geladenen Bild
-          let data_ved_of_id = mutation.target.dataset.ved;
-          let pic_a = document.querySelectorAll('[data-ved="' + data_ved_of_id + '"]')[0];
-
-          if ( !(pic_a.getAttribute("data-prepared")==="true") ) {
-            attr = document.createAttribute("data-prepared");
-            attr.value="true";
-            pic_a.setAttributeNode(attr);
-
-            prepare_a(pic_a);
-        }
-
+    let loaded_imgs;
+    for (mutation of mutations) {
+      if (mutation.target.tagName=="IMG") {
+        if ( !(mutation.target.getAttribute("data-prepariert")=="true") ) { //wenn noch nicht prepariert
+          prepare_img(mutation.target);
+          mark_img_as_prepared(mutation.target);
           }
       }
-    });
+    }
+  });
 
 var body_container = document.body;
 observer.observe(body_container, {
   subtree: true,
   attributes: true
 });
+
 
 //--------------------------helping functions---------------------------------------------
 
@@ -88,80 +88,61 @@ function get_clipboard_content_from_HTMLImage(htmlImage) {
   return "(" + src + " / "+ date_string + " " + time_string + ")";
 }
 
-function get_clipboard_content_from_a(htmlA) {
-  let child_image = htmlA.getElementsByTagName("img")[0];
-  src = child_image.src;
-
-  today = new Date();
-            day = today.getDate();
-            month = parseInt(today.getMonth(), 10) + 1;
-            year = today.getFullYear();
-            date_string = day + ". " + month + ". " + year;
-
-            hours = today.getHours();
-            minutes = today.getMinutes();
-            if (minutes<10) time_string = hours + ":" + "0" +  minutes;
-            else time_string = hours + ":" + minutes;
-  return "(" + src + " / "+ date_string + " " + time_string + ")";
-}
-
 function copy_to_clipboard(string) {
   navigator.clipboard.writeText(string);
 }
 
-function prepare_a(pic_a) {
+function prepare_img(img) {
+
+          let parent_node = img.parentNode;
+          let wrapper = document.createElement("div");
+          let attr = document.createAttribute("class");
+          attr.value="added_afterwards"
+          wrapper.setAttributeNode(attr);
+          parent_node.replaceChild(wrapper, img);
+          wrapper.appendChild(img);
 
           let button = document.createElement("BUTTON");
-          let icon = document.createElement("img");
-            icon.src=chrome.extension.getURL("rsc/clipboard_icon.png");
-            icon.style.width="70%";
-            icon.style.height="80%";
-            icon.style.margin="auto";
-            icon.style.position="absolute";
-            icon.style.left=0;
-            icon.style.right=0;
-            icon.style.top=0;
-            icon.style.bottom=0;
-            button.appendChild(icon);
-          button.style.position="absolute";
-          button.style.right=0;
-          button.style.bottom=0;
-          button.style.width="25px";
-          button.style.height="25px";
-          button.style.display="none";
+            button.style.position="absolute";
+            button.style.left=0;
+            button.style.top=0;
+            button.style.width="25px";
+            button.style.height="25px";
+            button.style.display="none";
+            button.addEventListener("click", function(event) { //Wenn Button gedrückt wird, dann wird Text ins Clipboard kopiert.
+              event.stopPropagation();
+              event.preventDefault();
+              event.cancelBubble = true;
+              event.stopImmediatePropagation();
 
-
-          button.addEventListener("click", function(event) { //Wenn Button gedrückt wird, dann wird Text ins Clipboard kopiert.
-            event.stopPropagation();
-            event.preventDefault();
-            event.cancelBubble = true;
-            event.stopImmediatePropagation();
-
-            clipboardcontent = get_clipboard_content_from_a(pic_a);
-            console.log(clipboardcontent);
-            copy_to_clipboard(clipboardcontent);
           });
-          pic_a.appendChild(button);
+          wrapper.appendChild(button);
 
-          pic_a.addEventListener("mouseenter", function() {
-              div_mouseover_event(pic_a);
+          wrapper.addEventListener("mouseenter", function() {
+              div_of_img_mouseover_event(wrapper);
           });
 
-          pic_a.addEventListener("mouseleave", function() {
-            div_mouseleave_event(pic_a);
+          wrapper.addEventListener("mouseleave", function() {
+              div_of_img_mouseleave_event(wrapper);
       });
-
 }
 
-function div_mouseover_event(a) {
-    let btns = a.getElementsByTagName("button");
+function mark_img_as_prepared(htmlImage) {
+  let attr = document.createAttribute("data-prepariert");
+  attr.value = "true";
+  htmlImage.setAttributeNode(attr);
+}
+
+
+function div_of_img_mouseover_event(div) {
+    let btns = div.getElementsByTagName("button");
     for (i of btns) {
       i.style.display="block";
     }
 }
 
-function div_mouseleave_event(a) {
-  let btns = a.getElementsByTagName("button");
+function div_of_img_mouseleave_event(div) {
+  let btns = div.getElementsByTagName("button");
   for (i of btns) {
     i.style.display="none";
   }
